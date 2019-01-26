ff70296f15c30e206f450a256baff32f0a33c243
Gaby Vazquez <gabs.vz@gmail.com>
2019-01-25 20:19:42 -0800

    Provider and store.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
-import List                 from '../List/List';
-import CreateBtn            from '../CreateBtn/CreateBtn';
-import CreateForm           from '../CreateForm/CreateForm';
+import React      from 'react';
+import List       from '../List/List';
+import CreateBtn  from '../CreateBtn/CreateBtn';
+import CreateForm from '../CreateForm/CreateForm';
 import './App.css';
+import PropTypes  from 'prop-types';
 
-class App extends Component {
-  state = {
-    isCreating: true,
-    questions: [
-      {
-        name: 'Person 1',
-        question: "Why didn't I get a name?",
-        id: 1,
-        votes: 0
-      },
-      {
-        name: 'Alita',
-        question: 'Can I fly?',
-        id: 2,
-        votes: 5
-      }
-    ]
-  };
-
-  // TODO: send open addQuestion, closeCreate, openCreate, upVote, downVote, deleteQuestion
-  render () {
-    return (
-      <div className="app">
-        {this.state.isCreating ?
-          <CreateForm
-            addQuestion={() => {}}
-            closeCreate={() => {}}/> :
-          <CreateBtn openCreate={() => {}}/>}
-        <List
-          questions={this.state.questions}
-          upVote={() => {}}
-          downVote={() => {}}
-          deleteQuestion={() => {}}/>
-      </div>
-    );
+let questions = [
+  {
+    name: 'Person 1',
+    question: "Why didn't I get a name?",
+    id: 1,
+    votes: 0
+  },
+  {
+    name: 'Alita',
+    question: 'Can I fly?',
+    id: 2,
+    votes: 5
   }
-}
+];
+
+const App = (props) => (
+  <div className="app">
+    {props.isCreating ?
+      <CreateForm
+        addQuestion={() => {}}
+        closeCreate={() => {}}/> :
+      <CreateBtn openCreate={() => {}}/>}
+    <List
+      questions={questions}
+      upVote={() => {}}
+      downVote={() => {}}
+      deleteQuestion={() => {}}/>
+  </div>
+);
+
+App.propTypes = {
+  isCreating: PropTypes.bool.isRequired
+};
+
 export default App;

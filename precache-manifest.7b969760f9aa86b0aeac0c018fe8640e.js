45a56cb8e57cd0ccf789f7531c88b03ec0a75178
Gaby Vazquez <gabs.vz@gmail.com>
2019-01-28 20:41:25 -0800

    Updates

---
+++ precache-manifest.7b969760f9aa86b0aeac0c018fe8640e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eae1339d5e57331f1a6",
+    "url": "/question-moderator/static/js/runtime~main.4eae1339.js"
+  },
+  {
+    "revision": "14a93c8bfbb244c8bc65",
+    "url": "/question-moderator/static/js/main.14a93c8b.chunk.js"
+  },
+  {
+    "revision": "7c79e16626814d4eda4a",
+    "url": "/question-moderator/static/js/1.7c79e166.chunk.js"
+  },
+  {
+    "revision": "14a93c8bfbb244c8bc65",
+    "url": "/question-moderator/static/css/main.ede9cc9d.chunk.css"
+  },
+  {
+    "revision": "4e80a507cc5d79a2007f5b43620eef1e",
+    "url": "/question-moderator/index.html"
+  }
+];
